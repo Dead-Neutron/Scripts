faf7b3b3bf8abe4cd9fdbae67510fa8c8970947e
Rajdeep Saha <rajdeepsaha41005@gmail.com>
2026-09-16 10:54:43 +0530

    updated the script1

--- script1.sh
+++ script1.sh
@@ -2,3 +2,16 @@
 
 # echo is same as the print statement.
 echo "Hello, $USER"
+
+echo -e "present working dir:\n$(pwd)"
+
+# creating and removing files
+mkdir new1 new2
+#changing the directory
+cd new1
+#creating the file
+touch hello.txt
+cd ..
+#recursive deletion of folder
+rm -r new1 new2
+
